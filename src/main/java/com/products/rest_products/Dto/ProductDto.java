d4eb7c2c3ec28f4d83b2816343582e2f4d65f0df
Denilson87 <denilsonmucavel@outlook.com>
2025-03-19 21:58:06 +0200

    building validations through DTO

---
+++ src/main/java/com/products/rest_products/Dto/ProductDto.java
@@ -0,0 +1,61 @@
+package com.products.rest_products.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ProductDto {
+    @NotBlank
+    private String ProductName;
+    @NotBlank
+    @Size(max =50)
+    private String ProductFamily;
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public String getProductFamily() {
+        return ProductFamily;
+    }
+
+    public void setProductFamily(String productFamily) {
+        ProductFamily = productFamily;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public String getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        Quantity = quantity;
+    }
+
+    public String getBarcode() {
+        return Barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        Barcode = barcode;
+    }
+
+    @NotBlank
+    private String Category;
+    @NotBlank
+    private String Quantity;
+    @NotBlank
+    private String Barcode;
+
+
+}
